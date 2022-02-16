ceb44bb9b75f8dbeddb9220856d0fe56c4aa9707
Kampaku <78536944+Kirinochka@users.noreply.github.com>
2022-02-16 14:23:24 +0300

    update

---
+++ src/js/GameSaving.js
@@ -0,0 +1,5 @@
+export default class GameSaving {
+  constructor(object) {
+    this.save = object;
+  }
+}
--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,11 +1,13 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 class GameSavingLoader {
   static async load() {
     const data = await read(); // возвращается Promise!
-    const value = await json(value); // возвращается Promise!
-    return value;
+    const value = await json(data); // возвращается Promise!
+    return new Promise((resolve) => resolve(new GameSaving(value)))
+      .then((res) => res.save);
   }
 }
 
